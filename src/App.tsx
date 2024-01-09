cd782522718fefed84159d2f83bc09df0220fd48
enzgamilla <laurenceangelogamilla@gmail.com>
2024-01-09 21:04:49 +0400

    add error handling in input form

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import "./App.css";
 import AllList from "./react-query/AllList";
 
 import TodoForm from "./react-query/TodoForm";
-import TodoList from "./react-query/TodoList";
 
 function App() {
   return (
